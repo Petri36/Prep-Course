6d3f3430a531c5333f265da74ad7089a826eed64
Agustin Petrini <petriniagustin41521@gmail.com>
2022-07-12 22:01:19 -0300

    quinto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -80,14 +80,20 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90)
+  return true;
+} else { 
+  return false;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50)
+  return true;
+} else {
+  Return false
 }
 
 function obtenerResto(x, y) {
@@ -100,7 +106,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+ 
 }
 
 function esImpar(num) {
