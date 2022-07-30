823765b7bab274c27ddf3b923a4ee3b2be095174
Agustin Petrini <petriniagustin41521@gmail.com>
2022-07-29 21:43:57 -0300

    deam

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -23,6 +23,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto = {};
+  for(var i = 0; i < string.length; i++){
+    if(objeto.hasOwnProperty(string[i])){
+      objeto [string[i]] = objeto [string[i]] +1;
+    } else {
+      objeto [string[i]] = 1
+    }
+  }
+   return objeto
 }
 
 
