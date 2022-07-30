67091023c2628eaeb300264cbdb9314545cf9228
Agustin Petrini <petriniagustin41521@gmail.com>
2022-07-29 22:39:11 -0300

    gfsdgfsd

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -82,7 +82,7 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-  var arr = ['You', 'are', 'beautifull', "looking"];
+  var arr = ['You', 'are', 'beautiful', "looking"];
   return arr.sort();
 }
 
